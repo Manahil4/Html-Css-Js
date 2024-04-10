cca6f71084613e5a034751a42afee87bce1fa869
Manahil Ejaz <manahilejaz16@gmail.com>
2024-04-10 11:55:32 -0700

    done today

--- js/lec6/script.js
+++ js/lec6/script.js
@@ -24,25 +24,31 @@
 // //html collection is like an array 
 // let paras=document.getElementsByTagName("p");
 // console.dir(paras);
-//querySelectorAll;returns Nodelist of same class or same tag elements in code. cannot use for ids as they are unique not multiple elemwts associated with a single id. querySelector;returns 1st node from nodelist of elements ,use with id, class , tag
-let frstelement=document.querySelector('p');
-console.dir(frstelement);
-let allelement=document.querySelectorAll('p');
-console.dir(allelement);
+////querySelectorAll;returns Nodelist of same class or same tag elements in code. cannot use for ids as they are unique not multiple elemwts associated with a single id. querySelector;returns 1st node from nodelist of elements ,use with id, class , tag
+// let frstelement=document.querySelector('p');
+// console.dir(frstelement);
+// let allelement=document.querySelectorAll('p');
+// console.dir(allelement);
 
-let frstcl=document.querySelector(".hd");
-console.dir(frstcl);
-let allcl=document.querySelectorAll(".hd");
-console.dir(allcl);
-let node_id=document.querySelector("#heading");
-console.dir(node_id);
-let butnId=document.querySelector("#butn");
-console.dir(butnId);
-console.log(butnId.tagName);//returns tagname of node 
-console.log(allelement.tagName);//undefine for collective nodelist elements name as tags can be different
+// let frstcl=document.querySelector(".hd");
+// console.dir(frstcl);
+// let allcl=document.querySelectorAll(".hd");
+// console.dir(allcl);
+// let node_id=document.querySelector("#heading");
+// console.dir(node_id);
+// let butnId=document.querySelector("#butn");
+// console.dir(butnId);
+// console.log(butnId.tagName);//returns tagname of node 
+// console.log(allelement.tagName);//undefine for collective nodelist elements name as tags can be different
 
-// when making dom tree ignore text and comment node only work on elements tag of html. fist child (leftmost) property, last child(rightmost), use for navigation
-console.dir(document.body.firstChild);//whitespaces are considered as text tag and if just after body there are spaces  considered as first child.as dom includes text, comment and html element for node
+// // when making dom tree ignore text and comment node only work on elements tag of html. fist child (leftmost) property, last child(rightmost), use for navigation
+// console.dir(document.body.firstChild);//whitespaces are considered as text tag and if just after body there are spaces  considered as first child.as dom includes text, comment and html element for node
 let divTag=document.querySelector("div").children
 console.dir(divTag);
 let sec=document.querySelector("section");
+console.log(sec.innerText);
+console.dir(sec.innerText);
+console.log(sec.innerHTML);
+console.dir(sec.innerHTML);
+sec.innerText='dsfs';
+console.log(sec.innerHTML, sec.innerText);
